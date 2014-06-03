208b57a494a5f5631ebe6f25df8710d3bf6595ec
Toby Inkster <mail@tobyinkster.co.uk>
2014-06-03 14:11:53 +0100

    bump version; changelog

--- lib/MooseX/ConstructInstance.pm
+++ lib/MooseX/ConstructInstance.pm
@@ -26,7 +26,7 @@ our @ISA = do {
 
 BEGIN {
 	$MooseX::ConstructInstance::AUTHORITY = 'cpan:TOBYINK';
-	$MooseX::ConstructInstance::VERSION   = '0.005';
+	$MooseX::ConstructInstance::VERSION   = '0.006';
 }
 
 # If push comes to shove, you can locally change the name
